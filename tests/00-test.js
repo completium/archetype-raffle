9fb2f95accb3433203ff11bf948a036a9f08b015
Benoit Rognier <benoit.rognier@edukera.com>
2022-02-15 19:27:07 +0100

    fix

--- tests/00-test.js
+++ tests/00-test.js
@@ -25,17 +25,16 @@ const JACKPOT           = "50tz"
 const TICKET_PRICE      = "5tz"
 const MIN_DURATION      = 60 * 60       // 1 hour
 const REVEAL_FEE        = [ '2', '10' ] // 20% (archetype rational type is 'pair int nat')
+const CHEST_TIME        = 10000000
 
 // Partial raffle key one 123456
-const TIME_1              = 3600
-const LOCKED_RAFFLE_KEY_1 = "0xc5ecdde89eb8c1e7aaeb85abb8f5e5cef3b4fa80c4aee2fccbf0a78ce0debaa5e9ddede3ddfbd9abdea28cc7dc99e6d3a9baf3cbae9adaaabc89cbc39e97e2c7a6cba99197d19ba09ddfd181afc997ffbcc5acb2d29ecbb698c2cacbdd83d1b4ced0bffe9cd78295b3fba4d9f9d5f4d4ec9ad3c7e1a8eeb9dba5cbd8a2dbf29af8e4a4c1e4b1edacf98fccefaef9fea4f0bacdd38ecbfe81c3f9839b9e9ab8fbf5f1eabac48a9f8ca7c588eefe94d1f18bd9bcee9aecde8dd285cf9098f4e1a7eec787f3a0e0ff9cd0ce8ec5a2a4e5ecb08fce899eb5baa397fabf90de9397cebc81bbdfb386e6b4da9fd8fdd19ed9f8d684c782b0aacfeebae4f6e7d1c5c1e6a093c68081cf83b991b4ecd7b38aee92deddcad79eb9abe0a0a0c6b5909dc58495f69445fff5ae9cefe8b8beb2fb86ccf5c9ad91989bdad8a3cfbedaffa2de8bf19dc6ac8cbc8a9584fa9f85f9ba958fc6bbc09ac8e7d5f0fdb98b86c1c7d59ad7c6dfc2d2cefaf5d9db909bf0e3acd3ccc792bc9bccbab4a4febda9b685dbc39ea2a4a7b69990d3abd8b9b3d7dbc581b984f3e08a98f7f7f0e697cc8dfd88edc8c3ca8dc3b2a9ccf6cdd6d0efcc848bc8ead5858bbabfcfc1c8ecea84fd9b96a5e4eabb8c918dafe6f78d83e8e1c2e5f8ee88a4ee8dcaeeafffebfcbbfda1e9eb86c582f2eedd9299cbc0a7fce083ced8c8ddb0e7eaacb696c1fccdadcdc8e3c6f7b9de84eece9bb7919094fef4fdf6efd8b1ba8bbecb9380add4f59ddbf9a19f95facc84e9d0a99bfa93f1fcc3a0fbde9b9ce0c7e8dec6e8d1dfa7dda6f490bb9580abfdbcc0e202e5ff731c3c17d080ee430edd30979a47aa653656e11e593800000015c2ca2a23b732a72932611618ad9ea324986377591e"
-const CHEST_KEY_1         = "0xa0aceddfb3c9fbe1b8c382c7d5a7dedbe2e5adf9edcfc3e9d084caa6aeb9818ff1e985cb9efe8fa089ceeaa0f5d0bcb583e2f29196f2d3908fffffdcda868faffcb78fb697e7eaf3e7dca9d4b5dda2c3e4f8adf8abf484ecae85f7d6e0f2d28cb69af1d7b19082e8d8d7ba96e7e1e0bb8ac9b9fcf0a9e5b7c1a499c4faf4c8a3a9c8e4d09aa780eac6cee1b78a97a3e983abf9a5f1e8d2a2a2b5e3bcb8c4effeb7a3a68a85a497cd91c9a2c096c3f596deb8d1aca3a5aff28effb8cfc9c7ced892e3a7c09deeb8c8ec9387a3b384b5c8bccaafc7a9a2c1cfd8c7becfd7d6828a9af8f4988fe4ead3b59ecfb8ff8cabf8be90d4c8bdbddfce9cd7c2bb81edc4b7ad80a59a978f8c9debe7aaf08cf0c588f3eaade6b9f4e4e6edf1ed9c9988e48d9ba0aa8f01d18bac92b886db9dd798b5f6fdc891a28da2c4c48da1918897a2b7c2dfa0b78ab8e291b68fb1a2bfa5e8b88e9cabb0b5b0feabcffc9cfeee888ac4afeed9dc8bf5a4eaa9ae89a3838cf6cfd4f8acff8fa7aef7a9889fbbc7d8f6dde4edf3e58096e580e299e5b082b9cf85f3fe8ac6c0998eb1bcbab9bfb8fba39faea7bce0f6fed9ea86dfdad58cf7cbc7fcc4ecf7e2e898d3b19582e38c8092b7e4a0cddc83eb8bc38d91fefed6be869496b8e4fc99d5fae5c6a2b2dcabe2a4ea85b68b87b182d7e8cac29fe0b9efd6d0eb999ffa98aaaf9bf09fe7c4b39d81db97e4e7bbaef0e3bfedd69d9089bc8d91b292afa6c8b389fc9fb7aaa8decab6d9b493a6eafaa5baffe8fb85f2d483ecd1f2d1e58f938df9d8d5e385fe96c5f58ae1e0b09bf2b3c2931f"
-const INVALID_CHEST_KEY_1 = "0xa0aceddfb3c9fbe1b8c382c7d5a7dedbe2e5adf9edcfc3e9d084caa6aeb9818ff1e985cb9efe8fa089ceeaa0f5d0bcb583e2f29196f2d3908fffffdcda868faffcb78fb697e7eaf3e7dca9d4b5dda2c3e4f8adf8abf484ecae85f7d6e0f2d28cb69af1d7b19082e8d8d7ba96e7e1e0bb8ac9b9fcf0a9e5b7c1a499c4faf4c8a3a9c8e4d09aa780eac6cee1b78a97a3e983abf9a5f1e8d2a2a2b5e3bcb8c4effeb7a3a68a85a497cd91c9a2c096c3f596deb8d1aca3a5aff28effb8cfc9c7ced892e3a7c09deeb8c8ec9387a3b384b5c8bccaafc7a9a2c1cfd8c7becfd7d6828a9af8f4988fe4ead3b59ecfb8ff8cabf8be90d4c8bdbddfce9cd7c2bb81edc4b7ad80a59a978f8c9debe7aaf08cf0c588f3eaade6b9f4e4e6edf1ed9c9988e48d9ba0aa8f01d18bac92b886db9dd798b5f6fdc891a28da2c4c48da1918897a2b7c2dfa0b78ab8e291b68fb1a2bfa5e8b88e9cabb0b5b0feabcffc9cfeee888ac4afeed9dc8bf5a4eaa9ae89a3838cf6cfd4f8acff8fa7aef7a9889fbbc7d8f6dde4edf3e58096e580e299e5b082b9cf85f3fe8ac6c0998eb1bcbab9bfb8fba39faea7bce0f6fed9ea86dfdad58cf7cbc7fcc4ecf7e2e898d3b19582e38c8092b7e4a0cddc83eb8bc38d91fefed6be869496b8e4fc99d5fae5c6a2b2dcabe2a4ea85b68b87b182d7e8cac29fe0b9efd6d0eb999ffa98aaaf9bf09fe7c4b39d81db97e4e7bbaef0e3bfedd69d9089bc8d91b292afa6c8b389fc9fb7aaa8decab6d9b493a6eafaa5baffe8fb85f2d483ecd1f2d1e58f938df9d8d5e385fe96c5f58ae1e0b09bf2b3c2931a"
+const LOCKED_RAFFLE_KEY_1 = "0x858b9aafb7d89ca6b9d9f2b683bfd1abd1dabddbc6cbafa48bbdd0ae9afec4e9a6f8ae81e985f8cbd49486ee88ccedc0f5cbcda9b1bfd5f6b8cef0938affb9ce93b186d7f6e7e0c8d3c5e0febeabaead97c4f6caace3dcf8fdee9ba0ba83d88e81a8dbeec5b5f3f7e58dd4c5e1af96a0e7b196a4c2b08980f0b5f0ebf7eca8a19eacdbeaf5faacdba5a6c4d8f995dfebc1c6bae4e9a1fd87b48bdcdafbf7f6b0b19fd9f8c6ec90fba48aa3dde0f0cbe4ccea88ca94c5ccb28f96b89ef0edbcac909afac28ce3def295f1d0d8eddef8df9fe7e7d1dea9a580e1e2cbed92ce8c8cdc8f91a394f68a86e6a0aceb97f8e6f28c90ae8cf3a6b3f3eba58ff4c6e3c2e0b4d1d38b94b5d98cdbda8081c0fa9bcceceaaea7faa897d7cbb694b38bdfb6e1f2c296f603d1ced3abebe5db8ea3e68defdd9ae9cbc6d4f0f3dafae1c78cffdcd6c4fde4ce83d8b2e3ac928bbaa6dc8cc6f8ddf5b8ed95bcb0d8fa9de9c39afd9aa6b08189ebd48addde8fbad6ceffe5e684ad9bede8c9bdbd9e98a3c1e49ed481c3cfad9cc880a795838ed8b8b1bdf6d4fee793e5bbf98c85e4cf89e990f8d5aae9c3e5fcd782bde2f2fe82b0d09ddfc2d8fd92da9af788ee85958286d8aecc91dac28fa9dcb1ca9addcd86e8e989d2e1b7b6fbe0c7afbe97d182ebade5fc9fd2f2baf19a898590f3c0c380f0d5c6aefdc48cefa9cebfd6aad1a083b8cafaa2ffbaa4cb8de2a9acebcbd5ca9ebae0de86e8b6bcc0e8a78f80d28fc397f3abffcd9a8ff3b2ee96d8cbe5dbd39582d6f3aa8ca4c6f1c3d69df3c1b8e0928edbf7c6e6dbe09bbee6df9806ab3f7ef06f7ac1868a1cfa8db0f7a7cd565a7464e5290dff00000015d8c58609c521ed06a5268079693dd0428a9824d377"
+const CHEST_KEY_1         = "0xedf6a3dbd5a0e8b2a3d2f5bab5c7def1b3a494ddf7d1c7f2a1ebb0fb82f291d893e7ccc9b4fadfb5ffbfecc3c1f0fdbed0d1bfecc592fed1edcd8ccfdee09ce8afebc6af8f8ce1a181cc87a089f7bebcc0e29eb0feb5defee3d8f5d0dccbd1d1e4b7a2d9f48dffcafad0bfda80ab8285a59ae482e9d9ac98d6bbc3fa98c5f5a8b8e8e294bc94bb96a6b0e7b297c4dc9a818d9ba7affcf1acedd6d7a8fbd6ecaccbeea7f7acbcd4a8ece6d7a89c9bdb87e7acfcbfefd6b09687ed92c8e3dcac948689bcaede9d88cbf3cba1ec969dd9ef9b97a790c5eca6918aaea5c1a4b0add1c59888ebacadf2dfaed5c9c4ccf5b9f2aeabaec2e1b39faca6dea7a2bf92c1e48acf9f85a086eba6c7f1b0ab99d8a8ee84cfcf8689eae7c2f7a3c09cdf90f1d5cd898551f2b3e49fd2dfadf19a80a59af3c8e5c1c2b0ddb2d8bcfeabbbf5f7b8fbe1dfa5edc6fbe0eaebfcefd8d3c7a7d6b796fe8dedcefd98cf8ab98af1daeabef6febc80e38bc7b795c7bfe8f9d7bcbc91cce58ebcdfa3faedd3d5fad7eabbad91bf8c9b8e8aeccfe095bbcaa7efc4adabc5b9a9abd595aea6c8859db29aa3f4d1d38db5e8ceb8d9eb9b99c892fcd9e2c4ec84dca7cbe4efedb2d2d3e5caa6d68ee681bbfb8ae0d2b7a2f39bb8e6bc919ac0d4c2e4bd98cc939992ebb49a8bb19bdf88e29ea6df90bb9bd2dc89b783bb8280839cc5d2f9b0f2a1ffdd9e9bdbc0ebf0aff8f78aa2efb6bdf7e7f9b9def4b9e5deabb096dabfd5f2d7dbd8deafb182b9b6fafae3c3f698ade28dc4e5cbc1a4f096bc9590dca5f095dea78585d1e389c5fefecc8131"
+const INVALID_CHEST_KEY_1 = "0xedf6a3dbd5a0e8b2a3d2f5bab5c7def1b3a494ddf7d1c7f2a1ebb0fb82f291d893e7ccc9b4fadfb5ffbfecc3c1f0fdbed0d1bfecc592fed1edcd8ccfdee09ce8afebc6af8f8ce1a181cc87a089f7bebcc0e29eb0feb5defee3d8f5d0dccbd1d1e4b7a2d9f48dffcafad0bfda80ab8285a59ae482e9d9ac98d6bbc3fa98c5f5a8b8e8e294bc94bb96a6b0e7b297c4dc9a818d9ba7affcf1acedd6d7a8fbd6ecaccbeea7f7acbcd4a8ece6d7a89c9bdb87e7acfcbfefd6b09687ed92c8e3dcac948689bcaede9d88cbf3cba1ec969dd9ef9b97a790c5eca6918aaea5c1a4b0add1c59888ebacadf2dfaed5c9c4ccf5b9f2aeabaec2e1b39faca6dea7a2bf92c1e48acf9f85a086eba6c7f1b0ab99d8a8ee84cfcf8689eae7c2f7a3c09cdf90f1d5cd898551f2b3e49fd2dfadf19a80a59af3c8e5c1c2b0ddb2d8bcfeabbbf5f7b8fbe1dfa5edc6fbe0eaebfcefd8d3c7a7d6b796fe8dedcefd98cf8ab98af1daeabef6febc80e38bc7b795c7bfe8f9d7bcbc91cce58ebcdfa3faedd3d5fad7eabbad91bf8c9b8e8aeccfe095bbcaa7efc4adabc5b9a9abd595aea6c8859db29aa3f4d1d38db5e8ceb8d9eb9b99c892fcd9e2c4ec84dca7cbe4efedb2d2d3e5caa6d68ee681bbfb8ae0d2b7a2f39bb8e6bc919ac0d4c2e4bd98cc939992ebb49a8bb19bdf88e29ea6df90bb9bd2dc89b783bb8280839cc5d2f9b0f2a1ffdd9e9bdbc0ebf0aff8f78aa2efb6bdf7e7f9b9def4b9e5deabb096dabfd5f2d7dbd8deafb182b9b6fafae3c3f698ade28dc4e5cbc1a4f096bc9590dca5f095dea78585d1e389c5fefecc813a"
 
 // Partial raffle key one 234567
-const TIME_2              = 7200
-const LOCKED_RAFFLE_KEY_2 = "0xeb9eb88682dda2bbc5f0b5b18d96b4a5aea29697bceceab8a6aac8bdc28feeddf990ccd5b7b1f684e781bff2c1e8ffb29db2a1f08cdcefced3fbd4eeeb99e9cb91cad8de9dc4d0e7d7e3aea0d6e79fffa8c3ecc0fbd2aaa682fdcad295a2a3e7fab4d7d3a0f09bcbe6d5ae86c0b0b28490cdb0d2cdfc88bad1ccfff78f9f8c9caab2c5aaed979f978cb890cca8b1e6c5a4dccba1eae1d1f9f3cbabd9c687858e8591d2de90ace29fa8b0f5a9e4f1cbbdafb2a384f1fee095afa6959e94adaad6cead93dafa9791f1dbccdda4b6b99f87eac09daecca9fcccfbabdd8daca18187e8b49980b8d19ecf80e3b2ddafb097dbc6d0f6e1bc95f886b6c29fcbe381a490d0ddb1cb8f97e29d8da5b8db91b2e695a6cf95bf8690ceddeba1f5bd9eb8d4d4c7a28e03cdb6fcc58efcac81c3d1e3e6e3cc8890e5b7c2edd3bca4c6e0eeefb4b8f7ebd5d1d3f8d48fca8ee0ead6fd82bed6d585daa0f8c7ffb6bc86999bf89a93b5b3a8b4b1e48bafdcd2bcc1ddbbb3ca95d8b6bde5bdb2d79afa8f83aecd83f7fac0eae79e8a85f8be87879482f0e4d8ef9dbfb4abc29b87e4c8ee84a1a885a78be8e1fac4d4f3fbd3aebae6f5c6b3e18cadf0fbfdd4f5bdf3a6a7c986ddd1cbf2babbcce0b5a2fcb8b4c6f58af3aadcabb0c9b585e3b9f2c4baffcba89aeb85d391a1d3948b80e4ebc2c4a490d7d3c4d9fdf889fbc58bd1f2b880ece5f2f397efaeeb91d4eac7ff8de2b2f3cafedbd8e6dbbaf3e2c39fa380cf82a2f2ab9b82e5ce89dc95a1c292838caaecf3cac888d2dafeadb2f2ec87dfe2abd2ba81bd90eadac8c1ec9c07f832f3aba3f1c35ab0e727d0ec49ba812ce82d9dfc7a12b000000015fee369d8b1577775510462074fe58b2865a8d344f3"
-const CHEST_KEY_2         = "0xa8f9eccdce9cacafd594de91c2bba2dab2a490ef84c9a2ede9e1ff9096d2aab9d1eee3ccf3dda9cdc699989bc9b09ce0bab99690f692eb87c3a9e895f7a3d6fbf1bad0b5d6c7e5dddaaed7b0d0c4e881fca589e6bfc8d2c280b082dd81fcbaacadfcf4cbe797f1eddef2858e8db2c2f8f69f84cbfafbcff2b2e5d4d8e4d594eb969fcdfee3f1aadbd490b8c1cee4a189b4ea89afd6cf89d5fdc0d7f0a8bc95acfca2d4fdd5f1c481e9a0e6a7d6de809892b48ed3ebc3849ccead94f5e191d1a4a8c1fdede780d9c9cace82fdffdda5af9dc3bbf0c1cccecdb1e0cfaaa9df9ec386c6c3bdded1b2b8808ed190a98db1c3cceff6c4f3a6bab3979ff687bb9ca4f59f9eaef1bea299b9ca8fa6e7d2f186e8c8abef84e7dcfbc5fea4dcb7b693fa8898eb8202a5f5fe8df7d183aee790ffe389befbe1d2a3b880bbdfccf2979a888182dbf8dca6efcde9ebf49ed0bea48baff2ded082f3958fbecee08087f089c4abeb92a9a7f081dccffdb4be8e82decde49c83f0c5aac0da9e8ebed8bbdd95c7b0bda1e58db6e0b7b7e8c4c7fbd5e4daa8afab8eabcdc7b194f693e1daaae4cccbfbdcd6bfd496ccacd8e09fa8b3a6c585d0f7abb6d897e9d685d5b0c2b492819ec48bb2fbe5d7c0b68acfaac3e2e1c486e5d0f480a1f28c8bb1fceef28b8caad2efe9f1fba9cbffcd96fcada9ea93c6878f8cdac4b2a3c795b4c9efbfa0b0b09984f2bbfaf4cafde191b1d8998796acfd93d8a1dac6f9ff878af1c9e2b0b3fccbfef6b0dd9cb0c49aedeefdbbebb99be896b8c29fdbfc93f7ec8a8980e89fefd99d8f99b4c1c9fa01"
+const LOCKED_RAFFLE_KEY_2 = "0xa3b6caaeb7acdcb7918591e6f7e8e0b8e7e4f3c1a486d9fac2c89cb1decbd8e0ef8297c9ff84c6b48bab96ccec8fb088bae7f5f1c7b3d7fae9a2a38bcdb4f8ff9eb6ec9588e8a2b8ceb18ff8e5d5b7fc92bda79bb6a3a0cfc68085fff7b394cba3b0a6a7bfa1d595bbe08093bb86f3aaf6cec695ae84c0b8b1c7bdd091ddfab1a4b2dae6e4a5eade96b4add0908ffbc8cb82c9d6c1b7cdfcf1f5c5db85ec9f8ed9e0bcd0f2c5d597d286d3edcbb59cf7ff86d295a7fc9fefd98acaf1a68cf8f2ffb3dcbcadfea29b9e83dba9aba58dd883c3d0d0eff7c1dadb86dda896e3d892bfe782d5bdfb92d38cc1cfecb2eaa4d4848cfee59bdcb7a9d4cbdc99d5d4f69ab6ac97e9ad9e81fe85e9c1dfd5c793b9bc889e90d1b4a7aee4c2e89f84b5c5efdedadbc304abc0f7a6ead0fba5f397fa82c4b5a0fb9ed68cc2d69ae498ea959997d4a0da88c9fecdb4899fbce792deccb79bade8f7fe90ccd9fde1ef98f9b682b7d2f3aca197918c9e8abdd5bac5f8c6fd86f1a599f5bfd988b8b589d7cde4dafac8a685f0c5e6f7ba99add8cea8c6b08d9bb9e4d6c2b4d680b58588e3dfe4a8b8fdd6d2a7d586f2a8f5a8a5bffce5b998b8f8dda38ec2d9d2cbddb38182fca8e8fef7f087a6c7d3d8ce96fceeb7daaceea784bcc6d393da8096a3b6a8a2e8bdb6db9fb2f7edffe6afafb1edfc9f98f1c5d3838db9f6b4eda3e8a5a3c2b9a286d3a18597f3c1aa96b5d1ad859291baf198fda3d99cd2dce2eabc8fb9fcd4a8cde9d68aa78ce89fcdf2c9d0e9dc95d2fd86deb88283a68fe9a9e2b4a1d8fc8f989c8497cbabd9c19eee078dfc51fd314e39b07d35ed76c113724714f1e9eb119b8c4400000015048c9bb6a0e20d7a1e874ea9314751bf105378fffa"
+const CHEST_KEY_2         = "0xf4e4f7bbd4b5d58ac6e4d8858bcebde9acced187a5e8ba8fbfaaf78ccf86f1959d9de9f1ead9ffca8fbdf0faf38cb9e7ebdcbdaef198bce0bcf7f6d2faddde899a83d8a9a59092e4c19bc6d0fad1d7e8daab9de093e8eda19cf7dc9786e1f19dead8eeca95a9a1f8a19595acefd1fa9c8093cd8ef6e1dab98984c5d29ac9d1e79ae2ec80b1bfa4da83f1a2f09f808cbdeb80d995a7cf9c86dc9687ed95ccd3b1bbebb2d4e4c4efdcfd88a2bbdad48fcfbbe3c7f8c783cbfeb9b58ba8fcfdb8f1cbd09dc186f0909ae6b2f2f7f4cccabcc9a9c986d5f5f1a3a69ad290b882c8b283bfd7e884f5d2dce6a8cbbcc5aae989e2e0a4b5bdd2d7998befc6f3ccdfc19a87f7c096829585a5e8f599edecd7b9c4b8eed9d6cbc7ba858b998293afc7df938caaab0b9e95dbc1e1eab3c8aaebd0d0f2d9e8a58da7a4ffcaf4bcd180c688d4ecce8bfac3d3cb92cc8ed9999fcfdc8a93bb91e2a5bd9298cd9ffbf4efa9f39ec2f78dd79deef8cca7d8d0c3a9b4d2b8fadfc2b88a819a98afceedd9e298c1bec2bdfbac92e2ecdbecfed985d0a5d2f997c794af909995e7c3b38eabebdb90b6bab9f5e6faf7bba39fe3c3a48f93c890ecf6c9b4da8a9792f39bddefff90bcac8fdbb790d4aebfd490a8e3f4e7b3ff9fd681cfb5a181e6f892c89185aeafa983c4b6f2cfb18b9fa1a8bdee84a18bc6fda4c586a1d4a1d6c5e3a7aea9ebaab199dfededecdbf6c4a3afd688bcafb897fff0d794caafe297c68190ede8bea688d7d3e6c1c0a9f097a0f59e82958cda8ab2c8cab2abf1e6b888b38ea5badf96a3f59f8b9bbbe0f2fce006"
 
 // Errors
 const errors = {
@@ -78,8 +77,8 @@ describe("Open Raffle", async () => {
     await expectToThrow(async () => {
       await raffle.open({
         arg : {
-          cd    : now,
-          d     : "Raffle test"
+          cd : now,
+          t  : CHEST_TIME
         },
         as : alice.pkh
       })
@@ -89,8 +88,8 @@ describe("Open Raffle", async () => {
     await expectToThrow(async () => {
       await raffle.open({
         arg : {
-          cd    : [now + MIN_DURATION - 10],
-          d     : "Raffle test"
+          cd : [now + MIN_DURATION - 10],
+          t  : CHEST_TIME
         },
         as : owner.pkh
       })
@@ -100,8 +99,8 @@ describe("Open Raffle", async () => {
     await expectToThrow(async () => {
       await raffle.open({
         arg : {
-          cd    : [now + MIN_DURATION + 10],
-          d     : "Raffle test"
+          cd : [now + MIN_DURATION + 10],
+          t  : CHEST_TIME
         },
         as : owner.pkh
       })
@@ -110,8 +109,8 @@ describe("Open Raffle", async () => {
   it("Admin successfully calls 'open' entrypoint.", async () => {
     await raffle.open({
       arg : {
-        cd    : [now + MIN_DURATION + 10],
-        d     : "Raffle test"
+        cd : [now + MIN_DURATION + 10],
+        t  : CHEST_TIME
       },
       as : owner.pkh,
       amount : JACKPOT
@@ -121,8 +120,8 @@ describe("Open Raffle", async () => {
     await expectToThrow(async () => {
       await raffle.open({
         arg : {
-          cd    : [now + MIN_DURATION + 10],
-          d     : "Raffle test"
+          cd : [now + MIN_DURATION + 10],
+          t  : CHEST_TIME
         },
         as : owner.pkh,
         amount : JACKPOT
@@ -135,8 +134,7 @@ describe("Test 'buy' entrypoint (at this point a raffle is open)", async () => {
     await expectToThrow(async () => {
       await raffle.buy({
         arg : {
-          lrk : LOCKED_RAFFLE_KEY_1,
-          t   : TIME_1
+          lrk : LOCKED_RAFFLE_KEY_1
         },
         as : alice.pkh
       })
@@ -147,8 +145,7 @@ describe("Test 'buy' entrypoint (at this point a raffle is open)", async () => {
     await expectToThrow(async () => {
       await raffle.buy({
         arg : {
-          lrk : LOCKED_RAFFLE_KEY_1,
-          t   : TIME_1
+          lrk : LOCKED_RAFFLE_KEY_1
         },
         as : alice.pkh,
         amount : TICKET_PRICE
@@ -159,8 +156,7 @@ describe("Test 'buy' entrypoint (at this point a raffle is open)", async () => {
   it("Alice successfully calls 'buy' entrypoint.", async () => {
     await raffle.buy({
       arg : {
-        lrk : LOCKED_RAFFLE_KEY_1,
-        t   : TIME_1
+        lrk : LOCKED_RAFFLE_KEY_1
       },
       as : alice.pkh,
       amount : TICKET_PRICE
@@ -170,8 +166,7 @@ describe("Test 'buy' entrypoint (at this point a raffle is open)", async () => {
     await expectToThrow(async () => {
       await raffle.buy({
         arg : {
-          lrk : LOCKED_RAFFLE_KEY_1,
-          t   : TIME_1
+          lrk : LOCKED_RAFFLE_KEY_1
         },
         as : alice.pkh,
         amount : TICKET_PRICE
@@ -181,8 +176,7 @@ describe("Test 'buy' entrypoint (at this point a raffle is open)", async () => {
   it("Jack successfully calls 'buy' entrypoint.", async () => {
     await raffle.buy({
       arg : {
-        lrk : LOCKED_RAFFLE_KEY_2,
-        t   : TIME_2
+        lrk : LOCKED_RAFFLE_KEY_2
       },
       as : jack.pkh,
       amount : TICKET_PRICE
