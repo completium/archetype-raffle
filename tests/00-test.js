e22805fd55a0555086137b189ec86228993db269
Benoit Rognier <benoit.rognier@edukera.com>
2022-02-22 14:24:44 +0100

    fix raffle contract and test

--- tests/00-test.js
+++ tests/00-test.js
@@ -30,8 +30,6 @@ const OneHour           = 60 * OneMinute
 // Initialise arguments
 const OPEN_BUY          = now + 10 * OneMinute
 const CLOSE_BUY         = OPEN_BUY + 10 * OneHour
-const CLOSE_REVEAL      = CLOSE_BUY + 10 * OneHour
-const OPEN_TRANSFER     = CLOSE_REVEAL + 10 * OneHour
 const CHEST_TIME        = 10000000                   // 20 seconds on standard computer
 const REVEAL_FEE        = [ '2', '10' ]              // 20% (archetype rational type is 'pair int nat')
 
@@ -44,25 +42,33 @@ const INVALID_CHEST_KEY_1 = "0xedf6a3dbd5a0e8b2a3d2f5bab5c7def1b3a494ddf7d1c7f2a
 const LOCKED_RAFFLE_KEY_2 = "0xa3b6caaeb7acdcb7918591e6f7e8e0b8e7e4f3c1a486d9fac2c89cb1decbd8e0ef8297c9ff84c6b48bab96ccec8fb088bae7f5f1c7b3d7fae9a2a38bcdb4f8ff9eb6ec9588e8a2b8ceb18ff8e5d5b7fc92bda79bb6a3a0cfc68085fff7b394cba3b0a6a7bfa1d595bbe08093bb86f3aaf6cec695ae84c0b8b1c7bdd091ddfab1a4b2dae6e4a5eade96b4add0908ffbc8cb82c9d6c1b7cdfcf1f5c5db85ec9f8ed9e0bcd0f2c5d597d286d3edcbb59cf7ff86d295a7fc9fefd98acaf1a68cf8f2ffb3dcbcadfea29b9e83dba9aba58dd883c3d0d0eff7c1dadb86dda896e3d892bfe782d5bdfb92d38cc1cfecb2eaa4d4848cfee59bdcb7a9d4cbdc99d5d4f69ab6ac97e9ad9e81fe85e9c1dfd5c793b9bc889e90d1b4a7aee4c2e89f84b5c5efdedadbc304abc0f7a6ead0fba5f397fa82c4b5a0fb9ed68cc2d69ae498ea959997d4a0da88c9fecdb4899fbce792deccb79bade8f7fe90ccd9fde1ef98f9b682b7d2f3aca197918c9e8abdd5bac5f8c6fd86f1a599f5bfd988b8b589d7cde4dafac8a685f0c5e6f7ba99add8cea8c6b08d9bb9e4d6c2b4d680b58588e3dfe4a8b8fdd6d2a7d586f2a8f5a8a5bffce5b998b8f8dda38ec2d9d2cbddb38182fca8e8fef7f087a6c7d3d8ce96fceeb7daaceea784bcc6d393da8096a3b6a8a2e8bdb6db9fb2f7edffe6afafb1edfc9f98f1c5d3838db9f6b4eda3e8a5a3c2b9a286d3a18597f3c1aa96b5d1ad859291baf198fda3d99cd2dce2eabc8fb9fcd4a8cde9d68aa78ce89fcdf2c9d0e9dc95d2fd86deb88283a68fe9a9e2b4a1d8fc8f989c8497cbabd9c19eee078dfc51fd314e39b07d35ed76c113724714f1e9eb119b8c4400000015048c9bb6a0e20d7a1e874ea9314751bf105378fffa"
 const CHEST_KEY_2         = "0xf4e4f7bbd4b5d58ac6e4d8858bcebde9acced187a5e8ba8fbfaaf78ccf86f1959d9de9f1ead9ffca8fbdf0faf38cb9e7ebdcbdaef198bce0bcf7f6d2faddde899a83d8a9a59092e4c19bc6d0fad1d7e8daab9de093e8eda19cf7dc9786e1f19dead8eeca95a9a1f8a19595acefd1fa9c8093cd8ef6e1dab98984c5d29ac9d1e79ae2ec80b1bfa4da83f1a2f09f808cbdeb80d995a7cf9c86dc9687ed95ccd3b1bbebb2d4e4c4efdcfd88a2bbdad48fcfbbe3c7f8c783cbfeb9b58ba8fcfdb8f1cbd09dc186f0909ae6b2f2f7f4cccabcc9a9c986d5f5f1a3a69ad290b882c8b283bfd7e884f5d2dce6a8cbbcc5aae989e2e0a4b5bdd2d7998befc6f3ccdfc19a87f7c096829585a5e8f599edecd7b9c4b8eed9d6cbc7ba858b998293afc7df938caaab0b9e95dbc1e1eab3c8aaebd0d0f2d9e8a58da7a4ffcaf4bcd180c688d4ecce8bfac3d3cb92cc8ed9999fcfdc8a93bb91e2a5bd9298cd9ffbf4efa9f39ec2f78dd79deef8cca7d8d0c3a9b4d2b8fadfc2b88a819a98afceedd9e298c1bec2bdfbac92e2ecdbecfed985d0a5d2f997c794af909995e7c3b38eabebdb90b6bab9f5e6faf7bba39fe3c3a48f93c890ecf6c9b4da8a9792f39bddefff90bcac8fdbb790d4aebfd490a8e3f4e7b3ff9fd681cfb5a181e6f892c89185aeafa983c4b6f2cfb18b9fa1a8bdee84a18bc6fda4c586a1d4a1d6c5e3a7aea9ebaab199dfededecdbf6c4a3afd688bcafb897fff0d794caafe297c68190ede8bea688d7d3e6c1c0a9f097a0f59e82958cda8ab2c8cab2abf1e6b888b38ea5badf96a3f59f8b9bbbe0f2fce006"
 
-// Partial raffle key 3 345678
-const LOCKED_RAFFLE_KEY_3 = "0x96ddfd9a8fefadcaf381a3c3e6e3ffd0d4bd8b91a6bcae9bb5b9cee4b2dfe2bfc6b38e8d8dbdccc2c5a4d692a6acf5eed9ccffa3afcae3ffdbd3bcc2bfafa6f789839498ffdbcad6dc9ee691eac085f0ffc7b5d2919c828dfec0c397e1a6ca88cfc4d1cfd6ecb1be98c2c2f08f9ff8a19692f9a5e8eb9fe3a8c691b6ac83cf91e5fba0aff3e1f9acc5e3f99ee7f582daf0f997a38f83b5b497849fe5abf8fdf4fa81839498fa81ae96b9d5eaa5d293a4e5a2d3ddeae7c8fff8eee985cfb79db1f7b0e4d1cac1f3e5f7f684dedcd3a6a89deecdfaff9aa7d7fb82b5ffeaa1d284afd6dc8ec2fbd3d0dd9982d099ff9c879499d1b3b5cca7e0dffab1c2dfdf8bcbbcb9e090e1dabfc0a3f19bf28bf7b8ca90fecfeff8aeabc6d2f7a695d0f998aae7e1b315c5f7f8eac994cdb9ec8688cbcacfb5d1f481debdcdab9ce6f1a9ac89a889d6b5a8d08981d6f3e6e1befbf998befcfdadc9bcdaaa9fe7e8c2cdefc2a98fbffc949ffdbcdfe2b2abdfc5c9cfc5da84809ff492a3fdfddf9b9c9df6b1b698fee3b3fca194a19693babcfdbbf2e584ded5caccd4df949cd4cfc9c5d9ddcc8ae7cc8ee79ad7d2f58efba7b484ab88e3b2e1c6dbc5b3a6dac5e7c49ac7b8f3c1fafb979c889f94f088c28589a68ebf89abf699e8dbd8c58fbedd84c1a5cedab6d39a85edfd99a5d6f9fbe4948f9b8496aab5b7e1898f85b681d7a4ab80a5efd68a8bd19b8bc989b7819193ffd2f5dfd6fed0ebffe088ea969cd8c7a0fcab9f91e79feb9dee83cbfdd9f9f8d3d2f7d397c8b6ebfbd2c2d2b3ebbff980eabea6aeca9d9ebadadf55e46c54db7ec19ee4173f9c1f8c65e13ac455bb612c8a38c4000000152f0ba5ccb59bd1b5c32f0db866d3c6cc6f2d0734f9"
-const CHEST_KEY_3         = "0xa8a1f7acdebaace79fbe8ba3a89da4888e9db9d6cdfef6b6bac887b981fdb0ccb2d5b896b69ad5adaaceebcab8b3cf8ba1e7d2aad98cedebd3dbe688dae1e3b7cb82bcdaa3ff96d9c7d3e1a582dc82ca83aef4d781debbccb7e3a0c49bc3c2b3a8f4abe48eb3dccba6bba3b097c3d5bcc6e5edf7ec91dce9bbd6a6d8a8c5f2828bafc6c1fafba7db96b9e7aa87e8cfcbd1c4bbabcad5e6f7fca48594c9ebeaffa7969bbbddc69acd9bbbe6d0b484c0e7a18fafd493c0b3c7898ecb8be2b8b8d9abe192c685cfcbd0f0f2d68297e0b6998ccbb2b1f39cf4dae1dcf1e1cfe1c3dba8da90c2f9de9ebc9b9c83bebbfbd4899ae9ab9b8aaa85fdefd1a3a792f09a809c8ddea09ae7e3cf98cae8f0e6a3c685dfd68380b5f99cd8cc9f8cd4b8e3cfadeec6c431e3abfc8ddc8cad8385cc9b89a7f086dfacbcefa4d6bdf2f1aac8a9dcb0c8df84b499bf85eeaeeba194fab0a7ffbcc4d6cff0c0b6d2d6c1d6e4f2f29e99b4dc86f4c5aff5a9c991da88dbf3ead5c19ec7c1a2bce1f5eaddfff3e8c4fbdaefc0fc96f2fcdbc696b6e6a7c9d8cafaff8de8dcd0f3cb98c3b4d5bb84e3b89992dac9b6eae291cef2c2bfb6ed98e3aba3c4e0abaad7e5c68a8ec8eba5d889d3eeefafa0d7d8d2d9bbe5d3f1ec91d587b1e7ecdfb9c5e1c2a0f6c9abbc899ad6aa8599b8f4bf95a9f7a1b2bf86b8bf9ac8bc888b9dd5a5aaf0c9e9fce4b99d81b2979c9d90b9e6b494eeb8aefca8fecd91cc9ca588c0ece399b6b791aecca4bf90e9d598eac59d8bc8eccabab0e680d898b1f4d9a6f5d3beb6c1c3f3c69ef4f584f680d990a447"
+/**
+ * @description Partial raffle key 3 is 345678; This chest has been generated with the wrong time value
+ * @command tezos-client hash data '345678' of type nat
+ * @command timelock-utils --lock --data 05008e992a --time 10000001
+ */
+const LOCKED_RAFFLE_KEY_3 = "0xedf2d6c8f8bd8cb3dbe1e3b2d2b9c6fea9b7ad95b9ccfab3969bbbd7a08fea92bcff9ee090cb85f69aebb7c6a19af0e3cfe8e48b99ccfcc1bdfc9d9cc4ed9bb8c19dede0a3b3e2b3afe185db96d0dec38dc19eb2f2c4e5f09ccee2d3dadedfc0f2a6b1deb4f7d89decc9a298b2a0bac0e7aafb9de8c5b8b6a4ccd895fd97d0acf482c3e6b6bf83dc92a1d3f990e5dba1fda18f97b9f9f9ea839bdbfab5d6958b89a18ba6a8e7acf4f4d1aa8897b494abb1f6bde1a9aae2f6a982bbc286eefdffa4d0bcf1c99bb49bcaa4dbc597869bb5aeb8dff2c58ae3d0dd8ee2929684dacfbec8d485e9a893b3a398f6c2f6fd8db7cc9fae95b1c997e7caf8b8f1c9efecd88adc8dfeec8588c9a79af6c4faa7d78ee2dd87aedcc68bb9a9a49ba7f2b0e4fdec81a7f401c1dd9386cfb384d8e4deee92c4f7c6e9aab0b2a4c396fec5b9d2dee3c7fce2dc9b82e4bfb586f5b49cc3dfe0a3a9a3e0cff1c0b5cb8fa6f8beebcab48ea8a0a9bbb58884d2e084b2a0c6edcd94f5fdd7d3b0bce883dce39b9296f8b682e5daadbeb7c9ba90af9cebdc8885a6dffec7b1cea5bd81b8fef8d7ebfea6eae29d8bcddcfafbb898dcaedb9c8892efa5aaee83f891f890bb8bd9a6bed6eeafd4a1d8effcbd83fe9ef1b3bfe1cdbdc1ddeff1f6c3a9b3edf2a4a3fdc7d380d5f1a3f9cfb4d1e3dfa398ddc8d6f7be88c1b1cf8ca2c0d5b6ecd09bf2e0e8ca98fae6dbf4e689cc9cedc4e287d78f8cef89d5c7de8da0adc996c5e1d58b93ae88d6c6abd6b897d8e5c6fcde93d8d6ada2e6c3eeb094bba5a2a4a4b7f5fe988dde93b9b0e4e6ddeecc03ee6328372b1a0f6366c1f6c2cb62b3415eb35a022ecaabfd00000015a491185fbe2229c6e0e38e057562014056222b2b57"
+/**
+ * @description Key is obtained with the correct chest time; it generates a 'bogus cypher' error
+ * @command timelock-utils --create-chest-key --chest ${LOCKED_RAFFLE_KEY_3} --time 10000000
+ */
+const CHEST_KEY_3         = "0xc0b0d29ddaf3bde590d9a9febfec9fb8a99ca4b8f4aff9aa8f85bccbdc99e786b0dd9ef588e0cf9c8ac9e8a6caa1aecdbdf2ccaeb49c9eec84aaecd9d782d1c5bcedf7d99cf4f2d2fa85fbbc99b5e8a186f9a6b4cdaac597e1acb88fa4ddf9f4ece980a1988fb394b383ff8adda7eff196ed9aa1dbfe9cc7aa9aadfaf0c4a686fc8a829de2d2a096e7b7b186c799959ae096b198a6c79fe2a4dfae9de3c2fab4f7ceb4afd598c8edc4cbc3c4d7f4d4feb4cac7c2d3e0c6c1ef8dedda9d9be1b0a7edd0f8ecebd3c4d497dfc4a6aff4aef5a1aceeee87fec9fec08d96cec4b3a08f8aeff086b1d79288dbb38089bfbeb583d590abc28dc4f0f2c6ebc3dc81a9e0bba3c29fbba2e8ebf5fcd78da0dab1cbafc490fcb8da9c92a98e85ac96e8b6fbc0dfecb8039fd18283ea98c38b98e2b093a6ed8eeca0a0b1c1bfd5cf84a0f7dbc9a7e983afacfbf2cfc5fced84b3ca81bee0a888d2acf5d7ecadf2d6faa0ef84bcdcecd7869c9eaaf395959fb6df9e9cac83e5fb97b5a39e9fadf9b787d8d1fca0f7bcf7a59ef3a5c9f2ecbb8ef7cdc0eff9b0b9abe0a9b9d2f087aae0df84abf3d8ffd9db9bcdf4e084e5a9cc9d88e8b2b1efb292bde1c8acd5d094b8fbd681f2f8dca380c7b0d79c82d9fda9ebb1e1e894d6f8a1b894a3a3f7d4f0fb95d1d7cdb19bc3f7e2bae193d5f496e2a1a2ad918bdfbb81c999e4e6fa8580f1cedfeac9dcd4a9fbcdd8c9c9b1fcecf3e89092f3c5d89bfda4bdeaa3d3f3d9d0b1aea69583bed5e58e9cb59ec3fa81fc94c099b08bc8f3e6f1f0d1d0fedc8d8af9868883c6c892dbce84b274"
 
 // Errors
 const errors = {
-  INVALID_CALLER          : '"InvalidCaller"',
-  INVALID_CLOSE_DATE      : '"INVALID_OPEN_CLOSE_BUY_REVEAL"',
-  INVALID_AMOUNT          : '"INVALID_AMOUNT"',
-  INVALID_TICKET_PRICE    : '"INVALID_TICKET_PRICE"',
-  PLAYER_ALREADY_EXISTS   : '(Pair "KeyExists" "player")',
-  BUY_CLOSED              : '"BUY_CLOSED"',
-  BUY_OPEN                : '"BUY_OPEN"',
-  INVALID_CHEST_KEY       : '"INVALID_CHEST_KEY"',
-  INVALID_STATE           : '"InvalidState"',
-  PLAYER_ALREADY_REVEALED : '"PLAYER_ALREADY_REVEALED"',
-  INVALID_REVEAL_FEE      : '"INVALID_REVEAL_FEE"',
-  TRANSFER_CLOSED         : '(Pair "InvalidCondition" "r8")',
-  INVALID_REVEAL_TIME     : '"INVALID_REVEAL_TIME"'
+  EXISTS_NOT_REVEALED      : '"EXISTS_NOT_REVEALED"',
+  INVALID_AMOUNT           : '"INVALID_AMOUNT"',
+  INVALID_CALLER           : '"InvalidCaller"',
+  INVALID_CHEST_KEY        : '"INVALID_CHEST_KEY"',
+  INVALID_OPEN_CLOSE_DATE  : '"INVALID_OPEN_CLOSE_BUY"',
+  INVALID_REVEAL_FEE       : '"INVALID_REVEAL_FEE"',
+  INVALID_REVEAL_TIME      : '"INVALID_REVEAL_TIME"',
+  INVALID_STATE            : '"InvalidState"',
+  INVALID_TICKET_PRICE     : '"INVALID_TICKET_PRICE"',
+  PLAYER_ALREADY_EXISTS    : '(Pair "KeyExists" "player")',
+  PLAYER_ALREADY_REVEALED  : '"PLAYER_ALREADY_REVEALED"',
+  RAFFLE_CLOSED            : '"RAFFLE_CLOSED"',
+  RAFFLE_OPEN              : '"RAFFLE_OPEN"',
 }
 
 const closeTo = (value, target, epsilon) => { return Math.abs(value - target) < epsilon }
@@ -93,8 +99,6 @@ describe("Open Raffle", async () => {
         arg : {
           ob : OPEN_BUY,
           cb : CLOSE_BUY,
-          cr : CLOSE_REVEAL,
-          ot : OPEN_TRANSFER,
           t  : CHEST_TIME,
           rf : REVEAL_FEE
         },
@@ -102,29 +106,25 @@ describe("Open Raffle", async () => {
       })
     }, errors.INVALID_CALLER)
   });
-  it("Admin unsuccessfully calls 'initialise' entrypoint with wrong 'close_buy'.", async () => {
+  it("Owner unsuccessfully calls 'initialise' entrypoint with wrong 'close_buy'.", async () => {
     await expectToThrow(async () => {
       await raffle.initialise({
         arg : {
           ob : OPEN_BUY,
           cb : OPEN_BUY,
-          cr : CLOSE_REVEAL,
-          ot : OPEN_TRANSFER,
           t  : CHEST_TIME,
           rf : REVEAL_FEE
         },
         as : owner.pkh
       })
-    }, errors.INVALID_CLOSE_DATE)
+    }, errors.INVALID_OPEN_CLOSE_DATE)
   });
-  it("Admin unsuccessfully calls 'initialise' entrypoint with wrong 'reveal_fee'.", async () => {
+  it("Owner unsuccessfully calls 'initialise' entrypoint with wrong 'reveal_fee'.", async () => {
     await expectToThrow(async () => {
       await raffle.initialise({
         arg : {
           ob : OPEN_BUY,
           cb : CLOSE_BUY,
-          cr : CLOSE_REVEAL,
-          ot : OPEN_TRANSFER,
           t  : CHEST_TIME,
           rf : [ '20', '10' ]
         },
@@ -132,14 +132,12 @@ describe("Open Raffle", async () => {
       })
     }, errors.INVALID_REVEAL_FEE)
   });
-  it("Admin unsuccessfully calls 'initialise' entrypoint by sending not enough tez to the contract.", async () => {
+  it("Owner unsuccessfully calls 'initialise' entrypoint by sending not enough tez to the contract.", async () => {
     await expectToThrow(async () => {
       await raffle.initialise({
         arg : {
           ob : OPEN_BUY,
           cb : CLOSE_BUY,
-          cr : CLOSE_REVEAL,
-          ot : OPEN_TRANSFER,
           t  : CHEST_TIME,
           rf : REVEAL_FEE
         },
@@ -147,13 +145,11 @@ describe("Open Raffle", async () => {
       })
     }, errors.INVALID_AMOUNT)
   });
-  it("Admin successfully calls 'initialise' entrypoint.", async () => {
+  it("Owner successfully calls 'initialise' entrypoint.", async () => {
     await raffle.initialise({
       arg : {
         ob : OPEN_BUY,
         cb : CLOSE_BUY,
-        cr : CLOSE_REVEAL,
-        ot : OPEN_TRANSFER,
         t  : CHEST_TIME,
         rf : REVEAL_FEE
       },
@@ -161,14 +157,12 @@ describe("Open Raffle", async () => {
       amount : JACKPOT
     })
   });
-  it("Admin unsuccessfully calls 'initialise' entrypoint because a raffle is already initialised.", async () => {
+  it("Owner unsuccessfully calls 'initialise' entrypoint because a raffle is already initialised.", async () => {
     await expectToThrow(async () => {
       await raffle.initialise({
         arg : {
           ob : OPEN_BUY,
           cb : CLOSE_BUY,
-          cr : CLOSE_REVEAL,
-          ot : OPEN_TRANSFER,
           t  : CHEST_TIME,
           rf : REVEAL_FEE
         },
@@ -200,7 +194,7 @@ describe("Test 'buy' entrypoint (at this point a raffle is open)", async () => {
         as : alice.pkh,
         amount : TICKET_PRICE
       })
-    }, errors.BUY_CLOSED)
+    }, errors.RAFFLE_CLOSED)
     setMockupNow(OPEN_BUY + 10);
   });
   it("Alice successfully calls 'buy' entrypoint.", async () => {
@@ -252,7 +246,7 @@ describe("Players reveal their raffle key (at this point a raffle is open and tw
         },
         as : alice.pkh
       })
-    }, errors.INVALID_REVEAL_TIME)
+    }, errors.RAFFLE_OPEN)
   });
   it("Alice unsuccessfully calls 'reveal' entrypoint because of an invalid chest key.", async () => {
     setMockupNow(CLOSE_BUY + 10);
@@ -289,39 +283,50 @@ describe("Players reveal their raffle key (at this point a raffle is open and tw
         as : alice.pkh
       })
     }, errors.PLAYER_ALREADY_REVEALED)
-  })
-  it("Owner successfully calls 'reveal' entrypoint to reveal Jack's raffle key.", async () => {
-    setMockupNow(CLOSE_REVEAL + OneHour);
-    await checkBalanceDelta(owner.pkh, d => closeTo(d, 1, 0.01), async () => {
+  });
+  it("Jack successfully calls 'reveal' entrypoint and gets the reveal fee.", async () => {
+    await checkBalanceDelta(owner.pkh,  0, async () => {
+    await checkBalanceDelta(jack.pkh, d => closeTo(d, 1, 0.01), async () => {
     await checkBalanceDelta(alice.pkh,   0, async () => {
-    await checkBalanceDelta(jack.pkh,    0, async () => {
       await raffle.reveal({
             arg : {
               addr : jack.pkh,
               k    : CHEST_KEY_2
             },
-            as : owner.pkh
+            as : jack.pkh
           })
     }) }) })
-  })
+  });
 });
 describe("Test 'transfer' entrypoint", async () => {
-  it("Owner unsuccessfully calls 'transfer' entrypoint because transfer is closed.", async () => {
-    setMockupNow(CLOSE_REVEAL - 1);
+  it("Owner unsucessfully calls 'transfer' entrypoint because Bob is not revealed.", async () => {
     await expectToThrow(async () => {
       await raffle.transfer({
         as : owner.pkh
       })
-    }, errors.TRANSFER_CLOSED)
-  })
+    }, errors.EXISTS_NOT_REVEALED)
+  });
+  it("Owner sucessfully calls 'reveal' entrypoint to remove Bob's chest, and gets the unlock reward.", async () => {
+    await checkBalanceDelta(owner.pkh, d => closeTo(d, 1, 0.01), async () => {
+    await checkBalanceDelta(alice.pkh,   0, async () => {
+    await checkBalanceDelta(jack.pkh,    0, async () => {
+      await raffle.reveal({
+            arg : {
+              addr : bob.pkh,
+              k    : CHEST_KEY_3
+            },
+            as : owner.pkh
+          })
+    }) }) })
+    // check that bob is removed from palyer
+    const storage = await raffle.getStorage();
+    assert (storage.player.size === 2)
+  });
   it("Owner sucessfully calls 'transfer' entrypoint to send the jackpot to Jack.", async () => {
-    setMockupNow(OPEN_TRANSFER + 1);
-    await checkBalanceDelta(jack.pkh, 63, async () => {
+    await checkBalanceDelta(jack.pkh, 62, async () => {
       await raffle.transfer({
         as : owner.pkh
       })
     })
-    const storage = await raffle.getStorage();
-    assert (storage.player.size === 2)
   })
 })
